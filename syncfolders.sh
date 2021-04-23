8997b4e18431e02470f7088b11ca6d1ee761e542
julen98 <julenramon792@gmail.com>
2021-04-23 08:53:30 +0200

    syncfolderonedir.sh changed to a functional script.

--- syncfolders.sh
+++ syncfolders.sh
@@ -1,9 +1,34 @@
-#!/bin/bash
-# Sincronitzacio en dos fases, començant pel directori mes antic
+#!/bin/sh
+exitcode=1
+MAIN=$1
+COPY=$2
 
-Dir1=$1
-Dir2=$2
+# Read the user input   
+#echo "Enter the pendrive name: "  
+#read pendrive
 
-if 
-exit 0
 
+#$COPY = /media/$USER/$pendrive/
+
+# Making a backup of the main folder
+echo "Making a backup..."
+rsync -ar --backup --suffix=`date +'.%F_%H-%M'` $MAIN /backupRsync/
+
+# Check if folders exist
+if [ -d "$MAIN" ] && [ -d "$COPY" ]; then 
+    exitcode=0 
+    if [ "$MAIN" -nt "$COPY" ]; then
+        rsync -avu --delete --inplace $MAIN $COPY;
+        #rsync --modify-window 1 -a --delete --no-o --no-p --no-g $MAIN $COPY;
+    elif [ "$COPY" -nt "$MAIN" ]; then
+        rsync -avu --delete --inplace $COPY $MAIN;
+        #rsync --modify-window 1 -a --delete --no-o --no-p --no-g $COPY $MAIN;
+    else
+        echo "Here is nothing to sync :)"
+    fi
+else
+echo "The folders does not exist."
+# If folders does not exist, exit with exitcode=1
+exit $exitcode
+fi
+exit $exitcode
